5c8f27fd7e6e27e633c5d0d03ea461cc19153ece
tomcallaghan560 <93492639+tomcallaghan560@users.noreply.github.com>
2023-11-28 21:05:15 +0000

    Defined testConstructorWithValidPaletteSizeGreaterThan1 in ColourTableTest.java.

--- src/test/java/com/colourtable/ColourTableTest.java
+++ src/test/java/com/colourtable/ColourTableTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ColourTableTest {
-
+    @Test
+    void testConstructorWithValidPaletteSizeGreaterThan1() {
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(1));
+    }
 }
 
