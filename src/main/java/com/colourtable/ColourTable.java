7598c294ce79777a4ca7a20c39ebc1fe2e67b3ae
tomcallaghan560 <93492639+tomcallaghan560@users.noreply.github.com>
2023-11-28 21:02:40 +0000

    Added Junit Jupiter API as a dependency along with JUnit 5 to pom.xml file. Created ColourTable in the main folder and ColourTableTest in the test folder. Imported org.junit.jupiter.api.Test and static org.junit.jupiter.api.Assertions.* into ColourTableTest then defined the packages and classes in the files.

---
+++ src/main/java/com/colourtable/ColourTable.java
@@ -0,0 +1,5 @@
+package com.colourtable;
+
+public class ColourTable
+{
+}
---
+++ src/test/java/com/colourtable/ColourTableTest.java
@@ -0,0 +1,10 @@
+package com.colourtable;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ColourTableTest {
+
+}
+
