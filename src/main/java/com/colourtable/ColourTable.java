5c5bbf0551f0f856efc9d5d67b9cd7b6291f4eea
tomcallaghan560 <93492639+tomcallaghan560@users.noreply.github.com>
2023-11-28 23:06:39 +0000

    Defined add and isValidRGB.

--- src/main/java/com/colourtable/ColourTable.java
+++ src/main/java/com/colourtable/ColourTable.java
@@ -23,4 +23,23 @@ public class ColourTable {
     private boolean isValidPaletteSize(int size) {
         return size > 1 && (size & (size - 1)) == 0 && size < 1025;
     }
+    private boolean isValidRGB(int rgbColor) {
+        // Check if the RGB color is a valid 24-bit value
+        int red = (rgbColor >> 16) & 0xFF;
+        int green = (rgbColor >> 8) & 0xFF;
+        int blue = rgbColor & 0xFF;
+
+        return red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255 && rgbColor >= 0;
+    }
+
+    public void add(int rgbColor) {
+
+        // Validate RGB color
+        if (!isValidRGB(rgbColor)) {
+            throw new IllegalArgumentException("Invalid RGB color value added to ColourTable.");
+        }
+
+        // Add the RGB color to the palette
+        palette.add(rgbColor);
+    }
 }
