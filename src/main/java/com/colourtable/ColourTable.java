8bd7256cf7f427e41945002f735c84cac0836998
tomcallaghan560 <93492639+tomcallaghan560@users.noreply.github.com>
2023-11-28 21:11:44 +0000

    Augmented ColourTable to make testConstructorWithValidPaletteSizeGreaterThan1 pass.

--- src/main/java/com/colourtable/ColourTable.java
+++ src/main/java/com/colourtable/ColourTable.java
@@ -1,5 +1,26 @@
 package com.colourtable;
 
-public class ColourTable
-{
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColourTable {
+    private final int paletteSize;
+    private final List<Integer> palette;
+
+    public ColourTable(int paletteSize) {
+        if (!isValidPaletteSize(paletteSize)) {
+            throw new IllegalArgumentException("Invalid palette size. Must be greater than 1");
+        }
+        this.paletteSize = paletteSize;
+        this.palette = new ArrayList<>(paletteSize);
+    }
+
+    public int getPaletteSize() {
+        return paletteSize;
+    }
+
+    private boolean isValidPaletteSize(int size) {
+        return size > 1;
+    }
 }
