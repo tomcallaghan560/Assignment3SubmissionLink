f809d617e5f4bf5c55187bf277ab566cc16d9951
tomcallaghan560 <93492639+tomcallaghan560@users.noreply.github.com>
2023-12-04 15:12:25 +0000

    Added docstrings and comments.

--- src/main/java/com/colourtable/ColourTable.java
+++ src/main/java/com/colourtable/ColourTable.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ColourTable {
+    // The maximum size of the colour palette.
     private final int paletteSize;
+    // The list to store the colours in the palette.
     private final List<Integer> palette;
 
+    /**
+     * Constructs a ColourTable with the specified size.
+     * The size must be a power of 2, between 2 and 1024 (inclusive).
+     *
+     * @param paletteSize The size of the palette.
+     * @throws IllegalArgumentException If the palette size is not a power of 2 or is out of the valid range.
+     */
     public ColourTable(int paletteSize) {
         if (!isValidPaletteSize(paletteSize)) {
             throw new IllegalArgumentException("Invalid palette size. Must be between 2 and 1024 and be a power of 2.");
@@ -15,13 +24,31 @@ public class ColourTable {
         this.palette = new ArrayList<>(paletteSize);
     }
 
+    /**
+     * Returns the size of the palette.
+     *
+     * @return The palette size.
+     */
     public int getPaletteSize() {
         return paletteSize;
     }
 
+    /**
+     * Validates if the given size is a power of 2 and within the range [2, 1024].
+     *
+     * @param size The size to check.
+     * @return true if the size is a valid palette size, false otherwise.
+     */
     private boolean isValidPaletteSize(int size) {
         return size > 1 && (size & (size - 1)) == 0 && size < 1025;
     }
+
+    /**
+     * Validates if the given RGB color is a valid 24-bit value.
+     *
+     * @param rgbColor The RGB color to validate.
+     * @return true if the RGB color is valid, false otherwise.
+     */
     private boolean isValidRGB(int rgbColor) {
         // Check if the RGB color is a valid 24-bit value
         int red = (rgbColor >> 16) & 0xFF;
@@ -31,8 +58,14 @@ public class ColourTable {
         return red >= 0 && red <= 255 && green >= 0 && green <= 255 && blue >= 0 && blue <= 255 && rgbColor >= 0;
     }
 
+    /**
+     * Adds a color to the palette.
+     *
+     * @param rgbColor The RGB color to add.
+     * @throws IllegalArgumentException If the RGB color is invalid or a duplicate.
+     * @throws IllegalStateException If the palette already contains the maximum number of colors.
+     */
     public void add(int rgbColor) {
-
         // Validate RGB color
         if (!isValidRGB(rgbColor)) {
             throw new IllegalArgumentException("Invalid RGB color value added to ColourTable.");
@@ -50,6 +83,11 @@ public class ColourTable {
         palette.add(rgbColor);
     }
 
+    /**
+     * Returns the number of colors currently in the palette.
+     *
+     * @return The number of colors in the palette.
+     */
     public int getNumberOfColours() {
         return palette.size();
     }
--- src/test/java/com/colourtable/ColourTableTest.java
+++ src/test/java/com/colourtable/ColourTableTest.java
@@ -1,21 +1,31 @@
 package com.colourtable;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ColourTableTest {
+    /**
+     * Test that the constructor throws an IllegalArgumentException for a palette size of 1,
+     * which is invalid because a valid palette size must be greater than 1.
+     */
     @Test
     void testConstructorWithValidPaletteSizeGreaterThan1() {
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(1));
     }
 
+    /**
+     * Test that the constructor throws an IllegalArgumentException for a palette size of 1025 or more,
+     * which is invalid because a valid palette size must be less than 1025.
+     */
     @Test
     void testConstructorWithValidPaletteSizeLessThan1025() {
-
         assertThrows(IllegalArgumentException.class, () -> new ColourTable(1025));
     }
 
+    /**
+     * Test that the constructor successfully creates a ColourTable object for all palette sizes
+     * that are powers of two and less than 1025.
+     */
     @Test
     void testConstructorWithValidPaletteSizePowerOfTwo() {
         int number = 2;
@@ -26,12 +36,15 @@ public class ColourTableTest {
         }
     }
 
+    /**
+     * Test that the constructor throws an IllegalArgumentException for palette sizes
+     * that are not powers of two and less than 1025.
+     */
     @Test
     void testConstructorWithInvalidPaletteSizeNotPowerOfTwo() {
         int number = 1;
 
         while (number < 1025) {
-            // Check if the number is not a power of two
             if ((number & (number - 1)) != 0) {
                 int finalNumber = number;
                 assertThrows(IllegalArgumentException.class, () -> new ColourTable(finalNumber));
@@ -40,6 +53,9 @@ public class ColourTableTest {
         }
     }
 
+    /**
+     * Test that adding a valid RGB colour to the ColourTable is successful.
+     */
     @Test
     void testAddValidRGBColour() {
         ColourTable colourTable = new ColourTable(4);
@@ -47,6 +63,9 @@ public class ColourTableTest {
         assertNotNull(colourTable);
     }
 
+    /**
+     * Test that the ColourTable correctly tracks the number of colours added.
+     */
     @Test
     void testNumberOfColoursAddedValid() {
         ColourTable colourTable = new ColourTable(4);
@@ -55,6 +74,9 @@ public class ColourTableTest {
         assertEquals(2, colourTable.getNumberOfColours());
     }
 
+    /**
+     * Test that adding a colour to a full ColourTable throws an IllegalStateException.
+     */
     @Test
     void testAddExceedingCapacity() {
         ColourTable colourTable = new ColourTable(2);
@@ -63,14 +85,20 @@ public class ColourTableTest {
         assertThrows(IllegalStateException.class, () -> colourTable.add(0x0000FF)); // Blue
     }
 
-
+    /**
+     * Test that adding an invalid RGB colour (outside the valid range) to the ColourTable
+     * throws an IllegalArgumentException.
+     */
     @Test
     void testAddInvalidRGBColour() {
         ColourTable colourTable = new ColourTable(8);
         assertThrows(IllegalArgumentException.class, () -> colourTable.add(-0x1FFFFFF));
     }
 
-
+    /**
+     * Test that adding a duplicate colour to the ColourTable is not allowed and
+     * throws an IllegalArgumentException.
+     */
     @Test
     void testAddDuplicateColourNotAllowed() {
         ColourTable colourTable = new ColourTable(8);
